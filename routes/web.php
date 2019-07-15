20e34daca86219a35f2c611c26daa835fbffd5fb
leo.liang <543492227@qq.com>
2019-07-15 14:31:38 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/', 'StaticPagesController@home')->name('home');//主页
+Route::get('/help', 'StaticPagesController@help')->name('help');//帮助页
+Route::get('/about', 'StaticPagesController@about')->name('about');//关于页
+Route::get('signup', 'UsersController@create')->name('signup');//注册
