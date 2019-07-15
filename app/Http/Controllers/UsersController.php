8758104887a7b1536c689f1d70e0334749f64171
leo.liang <543492227@qq.com>
2019-07-15 15:07:44 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /**
+     * 注册用户的页面
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|\think\response\View
+     */
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user){
+        return view('users.show',compact('user'));
     }
 }
