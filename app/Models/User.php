8e670838954b726c5158f4789e88996bf1c5450d
leo.liang <543492227@qq.com>
2019-07-16 11:45:30 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(status::class);
     }
+
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
 }
