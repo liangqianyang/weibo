038ee5f05fe0fb3e177287eb9fb37a1492df96f1
leo.liang <543492227@qq.com>
2019-07-16 11:41:17 +0800

    发布微博

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use App\Models\Status;
+
+class StatusesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
+     * 创建微博
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+
+
+        $this->validate($request, [
+            'content' => 'required|max:500'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]);
+        session()->flash('success', '发布成功!');
+        return redirect()->back();
+    }
+}
